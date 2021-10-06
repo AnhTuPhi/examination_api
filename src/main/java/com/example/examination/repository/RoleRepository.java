c601031ee24aee4c972cd6a75098029121f422f5
dev1 <dev1@oceantech.vn>
2021-10-07 00:58:18 +0700

    first commit

---
+++ src/main/java/com/example/examination/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.example.examination.repository;
+
+import com.example.examination.entity.ERole;
+import com.example.examination.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
+
+
